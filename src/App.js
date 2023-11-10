eb54f9d231416e240abf58e46e0a1fc46be253c9
VijayAndluri <150114081+VijayAndluri@users.noreply.github.com>
2023-11-10 11:18:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
+import HeroSection from './herosection';
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Services = () => <div>Services Page</div>;
+const Contact = () => <div>Contact Page</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </div>
+    </Router>
+    <HeroSection/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/Herosection.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <h1>Welcome to Our Appointment Booking System</h1>
+      <p>Saving time and money with seamless appointment scheduling.</p>
+      <button>Book Now</button>
+    </div>
+  );
+};
+
+export default HeroSection;
+
